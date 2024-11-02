dbae4b49c23584d743d132eda2b7a0d5d1acd1aa
stevenhsu22 <a88240108@gmail.com>
2024-11-02 14:23:46 +0900

    finished course

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // pnpm install next@canary
+  experimental: {
+    ppr: 'incremental',
+  },
 };
 
 export default nextConfig;
